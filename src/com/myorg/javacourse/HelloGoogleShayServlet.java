5daf69cb82084cc8c73f50714c7c5ce76de33dfb
shayshwartz28 <shay.shwartz@gmail.com>
2015-03-12 09:23:43 +0200

    changed body of index.html to "hello shay shwartz"

--- src/com/myorg/javacourse/HelloGoogleShayServlet.java
+++ src/com/myorg/javacourse/HelloGoogleShayServlet.java
@@ -8,6 +8,6 @@ public class HelloGoogleShayServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello Shay, world");
+		resp.getWriter().println("Hello, world  ");
 	}
 }
