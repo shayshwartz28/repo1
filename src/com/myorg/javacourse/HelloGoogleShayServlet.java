62732d3cf6cd3e74c1a955f73fb928b24d06bf27
shayshwartz28 <shay.shwartz@gmail.com>
2015-04-02 09:05:56 +0300

    Exercise 02 - Math 1. change text of link (in index.html) to “Exercise 02 ­ Math” 2. Define int variables: num1, num2, num3. 3. Assign them values of: 4,3,7 4. Use these variables (with these values) with any arithmetic operation to create following result: 49 5. Print the full exercise to screen, e.g.: “Result of 5*2=10” , but make sure not to use digits when you print the exercise to screen​. Use only the variables names.

--- src/com/myorg/javacourse/HelloGoogleShayServlet.java
+++ src/com/myorg/javacourse/HelloGoogleShayServlet.java
@@ -7,7 +7,10 @@ import javax.servlet.http.*;
 public class HelloGoogleShayServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world  ");
+		int num1 = 4, num2 = 3, num3 = 7;
+		String resultMsg = "<h1>Result of (" + num1 + " + " + num2 + ") * " + num3 + " = " + (num1 + num2)*num3 +"</h1>"; 
+	
+		resp.setContentType("text/html");
+		resp.getWriter().println(resultMsg);
 	}
 }
